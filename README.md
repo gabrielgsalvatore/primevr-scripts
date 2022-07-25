6874672bab53a3c46f5aa5ed7a7022d7be744f38
gabriel.salvador <gabriel.salvador@senior.com.br>
2022-07-25 20:30:37 -0300

    new script for the double action ejector rod and some adjustments to folder structures

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # primevr-scripts
 Feel free to use these scripts for your weapons, pardon the poor organization never meant for this to turn into an actual repository, but might improve it later on.
 
-the .dlls for use with meatkit can be located inside "..\primevr-scripts\Scripts\Project1\bin\Meatkit\net35\PrimeVrLematScripts.dll"
+the .dlls for use with meatkit can be located inside "..\primevr-scripts\Scripts\Releases\MeatKit\PrimeVrLematScripts.dll"
 
--- Scripts/Project1/obj/Release/net35/Project1.AssemblyInfo.cs
+++ Scripts/Project1/obj/Release/net35/Project1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeVrLematScripts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
-[assembly: System.Reflection.AssemblyDescriptionAttribute("testlemat")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Prime Scripts for H3VR Guns")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeVrLematScripts")]
